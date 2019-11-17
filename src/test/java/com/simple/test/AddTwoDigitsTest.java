c8f61c7482c22d5676dd8b7a8640906a7ef2eaeb
t3rmin41 <t33rmin41@gmail.com>
2019-11-17 23:00:16 +0200

    Assertion added

--- src/test/java/com/simple/test/AddTwoDigitsTest.java
+++ src/test/java/com/simple/test/AddTwoDigitsTest.java
@@ -2,16 +2,20 @@ package com.simple.test;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AddTwoDigitsTest {
 
+
     private int addTwoDigits(int n) {
+        //Given a two-digit integer n, return the sum of its digits
         return n / 10 + n % 10;
     }
 
     @Test
     public void testAddTwoDigits() {
-        int n = 29;
-        addTwoDigits(n);
+        int n = 29; // 2 + 9 = 11
+        assertEquals(11, addTwoDigits(n));
     }
 
 }
