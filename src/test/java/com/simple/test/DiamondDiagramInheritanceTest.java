3488b93c14c897b49d6b56944302fa46b47a4ece
t3rmin41 <t33rmin41@gmail.com>
2019-12-14 13:00:37 +0200

    Explanation of diamond diagram inheritance added

--- src/test/java/com/simple/test/DiamondDiagramInheritanceTest.java
+++ src/test/java/com/simple/test/DiamondDiagramInheritanceTest.java
@@ -24,6 +24,7 @@ public class DiamondDiagramInheritanceTest {
     }
 
     public interface OrganicDoughFactory extends DoughFactory {
+//    public interface OrganicDoughFactory { // creates compile-time error: class OrganicPizza inherits unrelated defaults for getDough from types Pizza and OrganicDoughFactory
         default String getDough() {
             return "pure untreated wheat dough";
         }
@@ -54,6 +55,12 @@ public class DiamondDiagramInheritanceTest {
         new PizzaOrder().orderPizza(Arrays.asList("Pepperoni", "Bell Pepper"));
         System.out.println();
         new OrganicPizzaOrder().orderPizza(Arrays.asList("Sausage", "Mushroom"));
+        // prints "Making pie with pure untreated wheat dough" though inherits from 'Pizza' which inherits from 'DoughFactory'
+        // which should print "insecticide treated wheat dough" because of "getDough()" call in Pizza. Why is that?
+        // It's because inherited call to overridden method "get_dough" is determined by OrganicPizza inherited types (not by Pizza's parent as intuitively many would think), and if
+        // the direct parent (in this case - OrganicPizza direct parent OrganicDoughFactory) has the method of the same contract
+        // [signature = method name and types parameters, contract = access modifier + return type + signature]
+        // then get_dough() will be called from direct parent
     }
 
 }
